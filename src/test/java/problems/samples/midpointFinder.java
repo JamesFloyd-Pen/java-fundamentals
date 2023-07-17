db668c42472dafc923130af0fbdb3f44961584f5
JamesFloyd-Pen <Commanderpip@gmail.com>
2023-07-17 14:41:04 -0400

    Update
    Just a update example

---
+++ src/test/java/problems/samples/midpointFinder.java
@@ -0,0 +1,18 @@
+package problems.samples;
+
+public class midpointFinder {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 25;
+
+        //The middle point
+        System.out.println(x + (25 - 1)/2);
+
+        //25 -1 = 24
+        // 24 divided by 2 equals  12
+        // 1 + 12 = 13
+
+
+    }
+    
+}
